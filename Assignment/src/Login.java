5fb9604295030082449ecfc6e18ac831e3ac3bf3
TrungTa03 <116551232+TrungTa03@users.noreply.github.com>
2022-10-31 18:52:43 +0800

    Update Login.java

--- Assignment/src/Login.java
+++ Assignment/src/Login.java
@@ -2,9 +2,24 @@
 ////both customers and staffs
 
 ////
+import java.util.Scanner;
 
 public class Login{
     public void main(String[] args){
-      
+        #-------------------------------------------------
+        
+        #initialization of scanner and relating variables
+        Scanner sc = new Scanner(System.in);
+        
+        #-------------------------------------------------
+        
+        #first interfacing of the app
+       
+        System.out.println("############################\n");
+        System.out.println("Welcome to MOBLIMA\n");
+        System.out.println("Please choose mode of login");
+        System.out.println("[1] Login as STAFF");
+        System.out.println("[2] Login as CUSTOMER");
+        System.out.println("############################\n");
     }
 }
