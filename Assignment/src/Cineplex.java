eda6b92c57fac7c2e62912bb16e8c0ccf03dd3fa
TrungTa03 <116551232+TrungTa03@users.noreply.github.com>
2022-11-06 00:56:36 +0800

    Update Cineplex.java

--- Assignment/src/Cineplex.java
+++ Assignment/src/Cineplex.java
@@ -1,105 +1,39 @@
-import java.util.Scanner;
-public class Cineplex {
-	
-	//#------------------------
-	//initiate relevant variables
-		
-	private String name;
-	private Movie[] movielist;
-	private Booking bookticket = new Booking();
-	Scanner sc = new Scanner(System.in);
+package MovieApp;
 
-	//#------------------------
-	//constructors of the Cinema
-	public void setmovielist(Movie[] list) {
-		this.movielist=list;
-	}
-	public void setname(String name) {
-		this.name=name;
-	}
-	public String getname() {
+/*
+ * Title: Cineplex
+ * Use: Object Cineplex which is the biggest structure of all
+ * ---------------------------------------
+ */
+import java.util.Scanner;
+
+public class Cineplex {
+	//#################################################################
+	//
+	//PART 1: INITIALIZE RELEVANT VARIABLES
+	//
+	//1.1: Initialize objects init
+	Scanner sc = new Scanner(System.in);
+	public String name;							//name of cineplex
+	public Cinema[] cinemaList;					//list of cinemas within the cineplex
+	
+	//#################################################################
+	//
+	//PART 2: CONSTRUCTORS OF THE CLASS
+	
+	public String getName() {
 		return this.name;
 	}
-	public void runCineplexshowtime(int size) {
-		int choice=1;
-		do {
-			try {
-				printmovietitles(size);
-				System.out.println("Enter Movie Index to view showtime:");
-				System.out.println("Enter 0 to exit:");
-				choice=Integer.parseInt(sc.nextLine());
-				if(choice ==0)
-					break;
-				while(choice<1 && choice>size) {
-					System.out.println("Invalid Input");
-					System.out.println("Enter Movie Index to view showtime:");
-					choice=Integer.parseInt(sc.nextLine());
-				}
-				viewseatings(choice-1);
-				
-			}
-			catch(Exception e) {
-				System.out.println("Invalid Input");
-			}
-		}while(choice!=0);
+	public void setName() {
+		String Inp;
+		System.out.println("Set name for this Cineplex: ");
+		Inp = sc.nextLine();
+		this.name = Inp;
 	}
-	//print only "NOW SHOWING" MOVIE
-	public void printmovietitles(int size) {
-		
-		for(int x =0; x<size; x++) {
-			System.out.println("Movie #" + (x+1) +" "+ movielist[x].getTitle());
-		}
-		
-	}
-	public void viewseatings(int moviechoice) {
-		boolean check=true;
-		int sel;
-		do {
-			try {
-			movielist[moviechoice].printShowtime();
-			System.out.println("Select Show Time to view Seat's Status");
-			System.out.println("Enter 0 to Exit");
-			sel=Integer.parseInt(sc.nextLine());
-			if(sel==0) {
-				System.out.println("Exiting...");
-				break;
-			}
-			if(sel>0 && sel<=movielist[moviechoice].getShowlistSize()) {
-				movielist[moviechoice].printSeatStatus(sel-1);
-				//checkifbooking(moviechoice,sel-1);
-			}
-			else
-				throw new Exception();
-			}
-			catch(Exception e) {
-				System.out.println("Invalid Input");
-			}
-		}while(check);
-	}
+	//##################################################################
+	//
+	//PART 3: FUNCTIONS
 	
-//	public void checkifbooking(int moviechoice, int showindex) {
-//		int sel=1;
-//		do {
-//			try {
-//				System.out.println("Do you want to book?");
-//				System.out.println("1: Book Tickets");
-//				System.out.println("2: Exit");
-//				sel=Integer.parseInt(sc.nextLine());
-//				switch(sel) {
-//				case(1):
-//					movielist[moviechoice].BookSeats(showindex, this.name);
-//					break;
-//				case(2):
-//					System.out.println("Exiting...");
-//					break;
-//				default:
-//					System.out.println("Invalid input");
-//				}
-//				}
-//				catch(Exception e) {
-//					System.out.println("Invalid Input");
-//				}
-//			}while(sel!=2);
-//		}
-
+	
+	
 }
